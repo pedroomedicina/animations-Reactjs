dcdbe54e5119235c02dacb4b3af53a806e68b58b
pedroomedicina <pedromedina190@gmail.com>
2019-05-26 03:26:19 -0400

    init

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Backdrop.css';
+
+const backdrop = (props) => {
+    const cssClasses = ['BackDrop', props.show ? 'BackdropOpen':'BackdropClosed'];
+    return <div className={cssClasses.join(" ")}></div>
+}
+
+export default backdrop;
